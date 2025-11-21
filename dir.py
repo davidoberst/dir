0cf1752bf51016ab442c06456c81dc55641d6e89
davidoberst <juandavidbustosperez82@gmail.com>
2025-11-21 10:35:41 -0500

    error management

--- dir.py
+++ dir.py
@@ -1,5 +1,6 @@
 # IMPORT LIBRRIES
 import os
+import time
 import pyfiglet
 
 # BANNER
@@ -9,33 +10,56 @@ print("v.0.0.1 | by davidoberst | https://github.com/davidoberst")
 print("-"*58)
 
 # SELECT PATH:
-userEntry = input("[::] Enter the folder path : ")
-os.chdir(userEntry)
-listpath = os.listdir(userEntry)
+while True: 
+ 
+ userEntry = input("[::] Enter the folder path : ")
+ try:
+   os.chdir(userEntry)
+   listpath = os.listdir(userEntry)
+   break
+ 
+ except FileNotFoundError:
+    time.sleep(1)
+    print("[!] Not a valid path.\n")
+    time.sleep(1)
+ except NotADirectoryError:
+    time.sleep(1)
+    print("[!] Not a valid path.\n")
+    time.sleep(1)
+ except PermissionError:
+    time.sleep(1)
+    print("[!] You dont have hacces to open this folder.\n")
+    time.sleep(1)
+ except OSError:
+    time.sleep(1)
+    print("[!] Not a valid path.\n")
+    time.sleep(1)
 
-def listFilesAndShowSize():
- for x in listpath:
+time.sleep(1)
+print("[::] Path founded")
+
+for x in listpath:
     full_path = os.path.join(userEntry, x) 
     size_bytes = os.path.getsize(full_path)
 
     if size_bytes < 1024:
         # less 1 KB → show bytes
-        print(f"{x}  {size_bytes} B")
+        print(f"{x}   |   {size_bytes} B")
 
     elif size_bytes >= 1024 and size_bytes < 1024**2:
         # between 1 KB and less  1 MB → show KB
         size_kb = size_bytes / 1024
-        print(f"{x}  {size_kb:.2f} KB")
+        print(f"{x}   |    {size_kb:.2f} KB")
 
     elif size_bytes >= 1024**2 and size_bytes < 1024**3:
         # between 1 MB and less 1 GB → show MB
         size_mb = size_bytes / (1024**2)
-        print(f"{x}  {size_mb:.2f} MB")
+        print(f"{x}   |   {size_mb:.2f} MB")
 
     elif size_bytes >= 1024**3:
         # 1 GB or more → show GB
         size_gb = size_bytes / (1024**3)
-        print(f"{x}  {size_gb:.2f} GB")
+        print(f"{x}   |   {size_gb:.2f} GB")
 
 
  
